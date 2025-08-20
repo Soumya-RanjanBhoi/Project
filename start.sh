f4448eff4e90e5d41c2b6713c7ac362a087cf417
Soumya Ranjan Bhoi <soumya6ranjan9@gmail.com>
2025-08-20 15:36:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ set -o errexit
 
 python backend/main.py &
 
-streamlit run frontend/app.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run frontent/app.py --server.port=$PORT --server.address=0.0.0.0
